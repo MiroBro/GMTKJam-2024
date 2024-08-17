12c185653e9f1edeb85198623696382b735bb26b
AdamPalmar <adampalmar@gmail.com>
2024-08-18 01:51:50 +0200

    Adding simple house with gravity

--- new-game-project/move_stuff.gd
+++ new-game-project/move_stuff.gd
@@ -5,15 +5,37 @@ extends Node3D
 
 
 func turn_project_into_colliders():
-	for child in project_root.get_children():
-		var rb = RigidBody3D.new()
-		rb.name = child.name
-		project_root.add_child(rb)
-		child.reparent(rb)
-
-		var cs = CollisionShape3D.new()
-		cs.make_convex_from_siblings()
-		rb.add_child(cs)
+	var counter = 0
+	for root_child: Node3D in reference_root.get_children():
+		var scene = null
+		if root_child.name.begins_with("roof") :
+			scene = load("res://building_blocks/roof.tscn")
+		elif root_child.name.begins_with("wall"):
+			scene = load("res://building_blocks/wall.tscn")	
+				
+		var instance = scene.instantiate()
+		for child in instance.get_children():
+			if child is CollisionShape3D:
+				var box_shape: BoxShape3D = child.shape
+				box_shape.size = reference_root.get_child(counter).scale
+			if child is MeshInstance3D:
+				var child_mesh: MeshInstance3D = child
+				#child_mesh.scale = reference_root.get_child(counter).scale 
+		project_root.get_child(counter).add_child(instance)
+		counter += 1	
+		#var rb = RigidBody3D.new()
+		#rb.name = child.name
+		#child.add_child(rb)
+		##child.reparent(rb)
+#
+		#var cs = CollisionShape3D.new()
+		#var shape = BoxShape3D.new()
+		#shape.size = Vector3(1,1,1)
+		#cs.shape = shape 
+			#
+		#
+		##cs.make_convex_from_siblings()
+		#child.add_child(cs)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -26,14 +48,20 @@ func _ready() -> void:
 	var project_children = project_root.get_children()
 	
 	for i in range(len(reference_children)):
-		var ref: Node3D = reference_children[i]
+		var ref: MeshInstance3D = reference_children[i]
 		var pr: Node3D = project_children[i]
 
-		var offset = ref.position
-		var rot = ref.rotation
-
-		pr.position = project_root.global_position + offset
-		pr.rotation = rot
+		#var offset = reference_root.global_position - ref.global_position
+		#var rot = ref.quaternion
+		
+	   
+		#pr.global_transform.origin = ref.global_transform.origin
+		pr.position = ref.position
+		#pr.scale = ref.scale
+		pr.rotation = ref.rotation
+		
+		#pr.global_position = project_root.global_position + offset
+		#pr.quaternion = rot
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -42,9 +70,16 @@ func _process(delta: float) -> void:
 		freeze_physics(false);
 	pass
 
+	pass # Replace with function body.
+
+
 
 func freeze_physics(frozen: bool) -> void:
-	var meshes = project_root.get_children();
-	for mesh: RigidBody3D in meshes:
-		mesh.freeze = frozen;
-	pass # Replace with function body.
+	var nodes: Array[Node] = project_root.get_children();
+	for node in nodes:
+		
+		for child in node.get_children():
+			print("CHILD NAME ",child.name)
+			if child is RigidBody3D:
+				child.freeze = frozen;
+		pass # Replace with function body.
