c095529f6bf9a39b79585795a9de1251d355de8a
Torben Nordtorp <torben.nordtorp@icloud.com>
2024-08-17 23:17:40 +0200

    improved blueprint

--- new-game-project/Scenes/blue_print_open_button.gd
+++ new-game-project/Scenes/blue_print_open_button.gd
@@ -8,30 +8,28 @@ var targetYSize = 0
 
 var startOffset = Vector2(0,0)
 var endOffset = Vector2(0,0)
-var t = 0
+var t = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	target = get_child(1)
 	
 	if (target):
-		var size : Vector2 = target.get_parent_area_size()
-		targetYSize = size[1]
-		target.set_position(Vector2(0, size[1]))
+		var sizeVec : Vector2 = target.get_parent_area_size()
+		targetYSize = sizeVec[1]
+		target.set_position(Vector2(0, targetYSize))
 		print(target.name)
 
 	pass # Replace with function body.
 
 # Dismiss the blueprint
 func close_blueprint() -> void:
-	
 	if (target):
 		t = 0
 		startOffset = Vector2(0,0)
 		endOffset = Vector2(0, targetYSize)
 		
-		target.set_position(Vector2(0, targetYSize))
-	
+		#target.set_position(Vector2(0, targetYSize))
 	pass
 	
 # Dismiss the blueprint
@@ -42,7 +40,7 @@ func open_blueprint() -> void:
 		endOffset = Vector2(0,0)
 		startOffset = Vector2(0, targetYSize)
 		
-		target.set_position(Vector2(0, targetYSize))
+		#target.set_position(Vector2(0, targetYSize))
 	
 	pass
 
@@ -52,29 +50,17 @@ func _process(delta: float) -> void:
 	if (t <= 1):
 		target.set_position(startOffset.lerp(endOffset, t))
 		t += delta * 2
-	
 	pass
 
 
 func _on_open_blueprint_pressed() -> void:
-	
 	if (target):
 		open_blueprint()
-
-				
 	pass # Replace with function body.
 
 
 func _on_blueprint_ui_mouse_exited() -> void:
-	close_blueprint()
-	pass # Replace with function body.
-
-
-func _on_blueprint_ui_gui_input(event: InputEvent) -> void:
-	if (event.is_pressed()):
-		close_blueprint()
-	if (event.is_action_pressed("")):
-		close_blueprint()
+	#close_blueprint()
 	pass # Replace with function body.
 
 
