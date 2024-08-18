13378f6a54199b560d214cab8ab37331737ad50b
MiroBro <miro.brodlova@gmail.com>
2024-08-18 17:33:43 +0200

    Fix Main Menu buttons

--- new-game-project/Scenes/main_menu.gd
+++ new-game-project/Scenes/main_menu.gd
@@ -4,24 +4,15 @@ extends Node2D
 
 func _on_play_pressed() -> void:
 	
-	$ClickSFX.play()
+	print("hi")
 	
-	SceneTransition.change_scene("res://Scenes/IntroScene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/IntroScene.tscn")
 	
 	pass # Replace with function body.
 
 
 func _on_quit_pressed() -> void:
-	$ClickSFX.play()
+	
 	get_tree().quit()
 	
 	pass # Replace with function body.
-
-
-func _on_play_mouse_entered() -> void:
-	$HoverSFX.play()
-	pass
-
-func _on_quit_mouse_entered() -> void:
-	$HoverSFX.play()
-	pass
