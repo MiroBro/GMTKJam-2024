d5cdfdcbc1ff44d3966b82f25c16da4ee2913ed7
Torben Nordtorp <torben.nordtorp@icloud.com>
2024-08-18 19:23:03 +0200

    fixed screenshake bug

--- new-game-project/camera_3d.gd
+++ new-game-project/camera_3d.gd
@@ -1,13 +1,13 @@
 extends Camera3D
 
 @export var decay = 0.9  # How quickly the shaking stops [0, 1].
-@export var max_offset = Vector2(0.01, 0.01)
+@export var max_offset = Vector2(0.03, 0.03)
 @export var max_roll = 0.01  # Maximum rotation in radians (use sparingly).
 
 var trauma = 0.0  # Current shake strength.
 var trauma_power = 2  # Trauma exponent. Use [2, 3].
 
-var target_pos = self.position
+var original_pos = self.position
 var target_rot = self.quaternion
 var offset = Vector3(0.0, 0.0, 0.0)
 
@@ -18,25 +18,26 @@ func add_trauma(amount):
 	trauma = min(trauma + amount, 1.0)
 
 func _process(delta: float):
-	position = lerp(position, target_pos + offset, delta * 32.0)
-	self.quaternion = lerp(self.quaternion, target_rot, 32.0 * delta)
+	var target_pos = original_pos + offset
+	position = lerp(position, target_pos, 1 - exp(-delta * 32) )
+	quaternion = lerp(quaternion, target_rot, 1 - exp(-delta * 32))
 
 	if trauma != 0.0:
-		trauma = clamp(trauma, 0.0, 1.0)
 		var it = 1.0 - exp(-delta)
-		trauma = max(trauma - decay * it, 0)
+		trauma = lerp(trauma, 0.0, it)
 		shake()
 
 func shake():
 	var amount = pow(trauma, trauma_power)
 	var angle = max_roll * amount * randf_range(-1, 1)
-	
 	var T = 1
 	angle = clamp(angle,-T,T)
 	#self.rotate_z(angle)
 	offset.x = max_offset.x * amount * randf_range(-1, 1)
 	offset.y = max_offset.y * amount * randf_range(-1, 1)
-	var X = 0.003
+	
+	var X = 0.03
+	
 	offset.x = clamp(offset.x, -X, X)
 	offset.y = clamp(offset.y, -X, X)
 
