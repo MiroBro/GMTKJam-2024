e3d48ac3024dadce05c86af2b3efeecba9930caf
MiroBro <miro.brodlova@gmail.com>
2024-08-18 18:24:29 +0200

    Add scene transition

--- new-game-project/Scripts/scene_transition.gd
+++ new-game-project/Scripts/scene_transition.gd
@@ -1,8 +1,13 @@
 extends CanvasLayer
 
-func change_scene(target: String) -> void:
+func change_scene2(target: String) -> void:
+	$AnimationPlayer.play("dissolve")
+	await $AnimationPlayer.animation_finished
+	get_tree().change_scene_to_file(target)
+	$AnimationPlayer.play_backwards("dissolve")
+	
+func change_scene(target: String):
 	$Dissolve_Rect/AnimationPlayer.play("dissolve")
-	if $Dissolve_Rect/AnimationPlayer.is_playing(): return  
-	#await ($Dissolve_Rect/AnimationPlayer,'animation_finished')
+	await $Dissolve_Rect/AnimationPlayer.animation_finished
 	get_tree().change_scene_to_file(target)
 	$Dissolve_Rect/AnimationPlayer.play_backwards("dissolve")
